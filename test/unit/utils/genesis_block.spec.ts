9220a9d4b62424b2dce3484c6c23e9d1550147ab
nagdahimanshu <himanshu.nagda@lightcurve.io>
2023-01-13 13:28:05 +0100

    Add TODO comment

--- test/unit/utils/genesis_block.spec.ts
+++ test/unit/utils/genesis_block.spec.ts
@@ -193,6 +193,7 @@ describe('Build assets/legacy', () => {
 
 		afterAll(async () => fs.removeSync(genesisBlockPath));
 
+		// TODO: This test case is failing, will be fixed once this issue https://github.com/LiskHQ/lisk-sdk/issues/8006 is resolved.
 		it('should create genesis block', async () => {
 			when(db.get)
 				.calledWith(`${DB_KEY_CHAIN_STATE}:${CHAIN_STATE_UNREGISTERED_ADDRESSES}`)
