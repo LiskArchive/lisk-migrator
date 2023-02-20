eeaa47324d193c9168885b371218cd0408cdf51d
nagdahimanshu <himanshu.nagda@lightcurve.io>
2023-02-20 20:09:44 +0100

    Add TODO comment

--- src/index.ts
+++ src/index.ts
@@ -274,6 +274,7 @@ class LiskMigrator extends Command {
 				cli.action.start('Starting lisk-core v4');
 				try {
 					const network = networkConstant?.name as string;
+					// TODO: Verify and update the implementation
 					await startLiskCore(configCoreV4, appVersion, client, { network });
 				} catch (err) {
 					this.error(`Failed to start lisk core v4. ${(err as { stack: string }).stack}`);
